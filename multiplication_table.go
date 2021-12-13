3c7035aa4aa4951851b4083469781538d61093fa
Yog <yog.panjarale@gmail.com>
2021-12-13 16:42:52 +0530

    multiplication table

---
+++ multiplication_table.go
@@ -0,0 +1,18 @@
+//make a cli game which take a number and print the multiplication table of that number
+package main
+import (
+	"fmt"
+	"os"
+)
+func main() {
+	fmt.Print("Enter a number: ")
+	var input int
+	_, err := fmt.Scanf("%d", &input)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("%d * %d = %d\n", input, i, input*i)
+	}
+}
